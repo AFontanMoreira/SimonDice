fb250efbdff6d99f1b27ce59f4eb46a5a58934f1
Alex <afontanmoreira@danielcastelao.org>
2021-10-18 11:17:14 +0200

    Declaracion de Array e inicializado

--- SimonDice/src/main/java/com/example/simondice/MainActivity.kt
+++ SimonDice/src/main/java/com/example/simondice/MainActivity.kt
@@ -16,35 +16,48 @@ class MainActivity : AppCompatActivity() {
 
 //array1=generado, array2=dado por el jugador
 
-        var array1= ArrayList<Int>()
-        var array2=ArrayList<Int>()
+        var array1 = ArrayList<Int>()
+        var array2 = ArrayList<Int>()
 
-        val rojo: Button=findViewById(R.id.rojo)
-        val azul: Button=findViewById(R.id.azul)
-        val amarillo: Button=findViewById(R.id.amarillo)
-        val verde: Button=findViewById(R.id.verde)
+        val rojo: Button = findViewById(R.id.rojo)
+        val azul: Button = findViewById(R.id.azul)
+        val amarillo: Button = findViewById(R.id.amarillo)
+        val verde: Button = findViewById(R.id.verde)
         val inicio: Button = findViewById(R.id.jugar)
-        val siguienteRonda: Button=findViewById(R.id.siguiente)
+        val siguienteRonda: Button = findViewById(R.id.siguiente)
 
-        var jugando=false
+        var jugando = false
+
+        //instrucciones para el boton de inicio (cambiar "jugando" a true, borrar los arrays que pudieran estar molestando con cosas dentro, y añadir el primer color al array 1)
+
+        inicio.setOnClickListener() {
+            Toast.makeText(applicationContext, "Inicio", Toast.LENGTH_SHORT).show()
+            array1.clear()
+            array2.clear()
+            jugando = true
+            array1.add(Random.nextInt(4) + 1)
+            mensageUsuario(array1)
+        }
 
         //muestra el array 1 con toasts
 
-        fun mensageUsuario(array:ArrayList<Int>) {
-
-            for (color in array) {
-                when (color) {
-                    1 ->
-                        Toast.makeText(applicationContext, "Rojo", Toast.LENGTH_SHORT).show()
-                    2 ->
-                        Toast.makeText(applicationContext, "Azul", Toast.LENGTH_SHORT).show()
-                    3 ->
-                        Toast.makeText(applicationContext,"Amarillo",Toast.LENGTH_SHORT).show()
-                    else ->
-                        Toast.makeText(applicationContext, "Verde", Toast.LENGTH_SHORT).show()
-                }
-            }
 
     }
+
+    private fun mensageUsuario(array: ArrayList<Int>) {
+
+        for (color in array) {
+            when (color) {
+                1 ->
+                    Toast.makeText(applicationContext, "Rojo", Toast.LENGTH_SHORT).show()
+                2 ->
+                    Toast.makeText(applicationContext, "Azul", Toast.LENGTH_SHORT).show()
+                3 ->
+                    Toast.makeText(applicationContext, "Amarillo", Toast.LENGTH_SHORT).show()
+                else ->
+                    Toast.makeText(applicationContext, "Verde", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
-}
+
